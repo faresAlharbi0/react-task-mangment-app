9b4a086d663b7fe48085c66a9cffcf46531cd570
fares alharbi <faresalharbi2k1@gmail.com>
2026-02-24 15:31:22 +0300

    fixes issues

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+<<<<<<< HEAD
   base: "/react-task-mangment-app/dist",
   plugins: [react()],
   build: {
     target: "esnext",
+=======
+  base: "/faresAlharbi0/react-task-mangment-app", // <--- add this
+  plugins: [react()],
+  build: {
+    target: "esnext", // ensures WASM support
+>>>>>>> 656bc80 (fixes issues)
   },
 });
