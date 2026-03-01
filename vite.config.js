c4365543fe2c9f02dc4e22d21193ccb7473edb5d
fares alharbi <faresalharbi2k1@gmail.com>
2026-03-01 12:12:41 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/react-task-mangment-app/dist",
   plugins: [react()],
+  build: {
+    target: "esnext",
+  },
 });
